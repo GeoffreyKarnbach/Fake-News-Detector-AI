c1d32702fa79a4f6fbdee30fbe7fa1367f32e6b2
DerGoyote <36735387+GeoffreyKarnbach@users.noreply.github.com>
2021-06-26 15:50:27 +0200

    Added coefficient for names and module

--- src/detection.py
+++ src/detection.py
@@ -1,3 +1,6 @@
+from name_recognition import get_human_names
+import math
+
 def get_exclamation_coefficient(text):
     # In average, with training data set, (2 times more ! for fake than non fake news) but only 1 in 3 articles (fake news) had !
     amount = text.count("!")   
@@ -14,21 +17,24 @@ def get_global_coefficient(text):
     non_fake_coeff = 0
 
     # Coefficient by exclamation point
-    exclam_coeff = get_exclamation_coefficient(sentence)
+    exclam_coeff = get_exclamation_coefficient(text)
     non_fake_coeff += exclam_coeff[0]
     fake_coeff += exclam_coeff[1]
 
     # Coefficient by total text length
-    total_length_coeff = get_total_length_coefficient(sentence)
+    total_length_coeff = get_total_length_coefficient(text)
     non_fake_coeff += total_length_coeff[0]
     fake_coeff += total_length_coeff[1]
+
+    # Coefficient by amount of names
+    amount_names = get_human_names(text)
+    fake_coeff += (1.2/math.exp(amount_names)+0.5)*4
+    non_fake_coeff += max((-2*((amount_names-8)**2)+30)*4,0)
 
     # Output
     return (non_fake_coeff,fake_coeff)
     
 
 if __name__ == "__main__":
-    sentence = "Hello World!"
-    print(get_global_coefficient(sentence))
-    
-    
+    text = "Hello World!"
+    print(get_global_coefficient(str(text)))    
---
+++ src/name_recognition.py
@@ -0,0 +1,29 @@
+import nltk
+from nameparser.parser import HumanName
+
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('maxent_ne_chunker')
+nltk.download('words')
+
+def get_human_names(text):
+    tokens = nltk.tokenize.word_tokenize(text)
+    pos = nltk.pos_tag(tokens)
+    sentt = nltk.ne_chunk(pos, binary = False)
+    person_list = []
+    person = []
+    name = ""
+    for subtree in sentt.subtrees(filter=lambda t: t.label() == 'PERSON'):
+        for leaf in subtree.leaves():
+            person.append(leaf[0])
+        if len(person) > 1: #avoid grabbing lone surnames
+            for part in person:
+                name += part + ' '
+            if name[:-1] not in person_list:
+                person_list.append(name[:-1])
+            name = ''
+        person = []
+
+    return len((person_list))
+
+
