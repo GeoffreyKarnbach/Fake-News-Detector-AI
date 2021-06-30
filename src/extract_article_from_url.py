332e46eda1124feb16994195617174083b996633
DerGoyote <36735387+GeoffreyKarnbach@users.noreply.github.com>
2021-06-30 13:41:01 +0200

    Newest version with updated article fetcher

--- src/extract_article_from_url.py
+++ src/extract_article_from_url.py
@@ -10,7 +10,8 @@ from bs4 import BeautifulSoup
 
 def get_text_from_url(url, output_file = "sample_input.txt"):
 
-    
+    h1_found = False
+
     res = requests.get(url)
     html_page = res.content
     soup = BeautifulSoup(html_page, 'html.parser')
@@ -25,19 +26,33 @@ def get_text_from_url(url, output_file = "sample_input.txt"):
         'html',
         'meta',
         'head', 
+        'header',
         'span', 
         'input',
         'style',
         'ul',
         'ol',
+        'li',
         'script',
+        'footer',
         'div',
         # there may be more elements you don't want, such as "style", etc.
     ]
 
     for t in text:
-        if t.parent.name not in blacklist:
-            output += '{} '.format(t)
+        if t.parent.name == "h1":
+            h1_found = True
+        
+
+        if h1_found:
+            if t.parent.name not in blacklist:
+                output += '{} '.format(t)
+    
+    if not h1_found:
+        for t in text:
+            if t.parent.name not in blacklist:
+                output += '{} '.format(t)
+
 
     with open("sample_input.txt","w", encoding="utf8") as f:
         f.write(output)
@@ -52,7 +67,14 @@ def get_title_from_url(url):
 
     output = ''
     for t in text:
+        
         if t.parent.name == "h1":
             output += '{} '.format(t)
 
     return output
+
+if __name__ == "__main__":
+    article = input("Enter article url: ")
+    print(get_title_from_url(article))
+    get_text_from_url(article)
+    
